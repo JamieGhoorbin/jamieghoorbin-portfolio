e27a409442781801e405e34a2da7eaab60868fee
Jamie Ghoorbin <jdg23@kent.ac.uk>
2021-02-27 00:28:49 +0000

    add babel to project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const testJamie = {
     name: "Jamie",
     location: "london",
+    abc: 1,
 };
 
+const two = {
+    ...testJamie,
+    location2: "Canterbury",
+    postcode: 2,
+}
+
 console.log(testJamie);
+console.log(two);
